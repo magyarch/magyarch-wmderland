ab21657e00603c2cc33e6bd30d0b22e39bf1db1c
bazeeel <deakfefe77@gmail.com>
2022-04-19 15:04:18 +0200

    fix polybar

---
+++ .local/bin/edconf.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+
+
+# Dmenu script for editing some of my more frequently edited config files.
+
+
+declare options=("alias
+bash
+bspwm
+profile
+polybar
+ratpoison
+sxhkd
+xprofile
+quit")
+
+choice=$(echo -e "${options[@]}" | rofi -dmenu -i -no-custom -p 'Edit a config file: ')
+
+case "$choice" in
+	quit)
+		echo "Program terminated." && exit 1
+	;;
+	alias)
+		choice="$HOME/.config/aliasrc"
+	;;
+	bash)
+		choice="$HOME/.bashrc"
+	;;
+	bspwm)
+		choice="$HOME/.config/bspwm/bspwmrc"
+	;;
+	profile)
+		choice="$HOME/.profile"
+	;;
+	vifm)
+		choice="$HOME/.config/vifm/vifmrc"
+	;;
+	polybar)
+		choice="$HOME/.config/polybar/config.ini"
+	;;
+        ratpoison)
+		choice="$HOME/.ratpoisonrc"
+	;;
+	sxhkd)
+		choice="$HOME/.config/sxhkd/sxhkdrc"
+	;;
+	xprofile)
+		choice="$HOME/.xprofile"
+	;;
+	*)
+		exit 1
+	;;
+esac
+subl -c "$choice"
